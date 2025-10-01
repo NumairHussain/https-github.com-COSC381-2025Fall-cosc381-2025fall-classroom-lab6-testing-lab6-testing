8a4e08a6727bd725eb0e2bac9f894b6e2708cd3d
Numair Hussain <123851476+NumairHussain@users.noreply.github.com>
2025-10-01 03:06:42 +0000

    fixed part 2

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -14,20 +14,9 @@ def sample_run_anonymizer(text: str, start: int, end: int):
         operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
     )
 
+    print(result)
     return(result)
 
-    # input should be:
-    # text: My name is Bond.
-    # start: 11
-    # end: 15
-    # 
-    # output should be:
-    # text: My name is BIP.
-    # items:
-    # [
-    #     {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
-    # ]
 
 if __name__ == "__main__": 
-    anonymizer = sample_run_anonymizer("My name is Bond.", 11, 15)
-    print(anonymizer)
+    sample_run_anonymizer("My name is Bond.", 11, 15)
